7cb14394df38212c9ab457f873d219fb0fafae91
Macintosh Helper <noreply@macintoshhelper.com>
2017-06-07 18:23:26 +0100

    Add voting and results display with gradient [WIP]

--- elm-files/Vote.elm
+++ elm-files/Vote.elm
@@ -1,6 +1,6 @@
 module Vote exposing (..)
 
-import Html exposing (Attribute, Html, button, div, h1, input, text, textarea)
+import Html exposing (Attribute, Html, button, div, h1, input, text, textarea, span)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 import Http
@@ -38,8 +38,8 @@ model =
     , questionId = "1"
     , canSelectMultiple = False
     , answers =
-        [ { text = "test answer", isSelected = False, votes = 0 }
-        , { text = "test answer 2", isSelected = False, votes = 0 }
+        [ { text = "test answer", isSelected = False, votes = 4 }
+        , { text = "test answer 2", isSelected = False, votes = 2 }
         ]
     , display = Voting
     }
@@ -102,7 +102,7 @@ update msg model =
             ( { model | answers = toggleSpecificAnswer indexToToggle model.answers }, Cmd.none )
 
         Vote ->
-            ( model, Cmd.none )
+            ( { model | display = Result }, Cmd.none )
 
 
 
@@ -114,13 +114,35 @@ renderAnswerButton index answer =
     button [ answerButtonClass answer.isSelected, onClick (ToggleAnswer index) ]
         [ text answer.text ]
 
+-- renderVoteGradient : Int -> String
+-- renderVoteGradient votes =
+--     let percentage = model.answers in
+--         "linear-gradient(90deg, green 50%, white 50%);"
+--     -- "linear-gradient(90deg, green 50%, white 50%);"
+
+renderResultAnswer : Int -> Answer -> Html Msg
+renderResultAnswer index answer =
+    div [ answerButtonClass False, style [("background", "linear-gradient(90deg, green 50%, white 50%)")] ]
+        [ --div [style [("width", "50%"), ("background", "#B1FFBD")]] []
+         span [] [text answer.text]
+        , span [] [text ("Votes: " ++ toString answer.votes) ]
+    ]
+
 
 view : Model -> Html Msg
 view model =
-    div [ containerClass ]
-        ([ h1 [ titleClass ] [ text model.question ] ]
-            ++ List.indexedMap renderAnswerButton model.answers
-            ++ [ button [ createButtonClass, onClick Vote ] [ text "Vote" ]
-               , div [] [ text model.questionId ]
-               ]
-        )
+    if model.display == Voting then
+        div [ containerClass ]
+            ([ h1 [ titleClass ] [ text model.question ] ]
+                ++ List.indexedMap renderAnswerButton model.answers
+                ++ [ button [ createButtonClass, onClick Vote ] [ text "Vote" ]
+                   , div [] [ text model.questionId ]
+                   ]
+            )
+    else
+        div [ containerClass ]
+            ([ h1 [ titleClass ] [ text model.question ] ]
+                ++ List.indexedMap renderResultAnswer model.answers
+                ++ [ div [] [ text model.questionId ]
+                   ]
+            )
