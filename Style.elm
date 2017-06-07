065046a0b82a289b6c751da0d7a0e506e5607fb0
Matt Lubel <matt_lubel@hotmail.com>
2017-06-07 15:26:06 +0100

    add voting page

--- Style.elm
+++ Style.elm
@@ -11,6 +11,10 @@ titleClass =
     class "tc mv5"
 
 
+
+-- create page
+
+
 questionClass =
     class "center f3 db w-90 br3 ba bw2 b--blue pa3 ma3"
 
@@ -21,3 +25,14 @@ answerClass =
 
 createButtonClass =
     class "center db w4 br-pill ba bw2 b--yellow bg-white pa3 ma4"
+
+
+
+-- Vote page
+
+
+answerButtonClass isSelected =
+    if isSelected == True then
+        class "center db w-90 center ba bg-green br3 pa3 ma3"
+    else
+        class "center db w-90 center ba bg-white br3 pa3 ma3"
