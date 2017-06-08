dd82dc1d022bcc1c06240cc5c590b676ab6871de
Matt Lubel <matt_lubel@hotmail.com>
2017-06-08 12:32:52 +0100

    change model structure

--- elm-files/Vote.elm
+++ elm-files/Vote.elm
@@ -1,15 +1,22 @@
 module Vote exposing (..)
 
-import Html exposing (Attribute, Html, button, div, h1, input, text, textarea, span)
+import Html exposing (Attribute, Html, button, div, h1, input, span, text, textarea)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode exposing (..)
-import Style exposing (..)
 import Navigation
+import Style exposing (..)
 
 
 -- MODEL
+
+
+type alias Question =
+    { id : String
+    , text : String
+    , answers : List Answer
+    }
 
 
 type alias Answer =
@@ -18,43 +25,42 @@ type alias Answer =
     , votes : Int
     }
 
+
 type Display
     = Voting
     | Result
 
 
 type alias Model =
-    { question : String
-    , questionId : String
-    , canSelectMultiple : Bool
-    , answers : List Answer
+    { question : Question
     , display : Display
     }
 
 
 model : Model
 model =
-    { question = "Is this test question useful or not?"
-    , questionId = "1"
-    , canSelectMultiple = False
-    , answers =
-        [ { text = "test answer", isSelected = False, votes = 4 }
-        , { text = "test answer 2", isSelected = False, votes = 2 }
-        , { text = "test answer 3", isSelected = False, votes = 20 }
-        , { text = "test answer 4", isSelected = False, votes = 15 }
-        , { text = "test answer 5", isSelected = False, votes = 6 }
-        ]
+    { question =
+        { text = "Is this test question useful or not?"
+        , id = "1"
+        , answers =
+            [ { text = "test answer", isSelected = False, votes = 4 }
+            , { text = "test answer 2", isSelected = False, votes = 2 }
+            , { text = "test answer 3", isSelected = False, votes = 20 }
+            , { text = "test answer 4", isSelected = False, votes = 15 }
+            , { text = "test answer 5", isSelected = False, votes = 6 }
+            ]
+        }
     , display = Voting
     }
 
 
-getQuestionData =
+getQuestionData questionId =
     Cmd.none
 
 
 init : Navigation.Location -> ( Model, Cmd Msg )
 init location =
-    ( { model | questionId = String.dropLeft 1 location.hash }, getQuestionData )
+  ( model, getQuestionData (String.dropLeft 1 location.hash) )
 
 
 main =
@@ -83,7 +89,7 @@ toggleSpecificAnswer indexToToggle answers =
 
 
 type Msg
-    = NewQuestion (Result Http.Error String)
+    = NewQuestion (Result Http.Error Question)
     | UrlChange Navigation.Location
     | ToggleAnswer Int
     | Vote
@@ -96,13 +102,25 @@ update msg model =
             ( { model | question = data }, Cmd.none )
 
         UrlChange location ->
-            ( { model | questionId = String.dropLeft 1 location.hash }, Cmd.none )
+            ( model, getQuestionData (String.dropLeft 1 location.hash) )
 
         NewQuestion (Err _) ->
             ( model, Cmd.none )
 
         ToggleAnswer indexToToggle ->
-            ( { model | answers = toggleSpecificAnswer indexToToggle model.answers }, Cmd.none )
+            let
+                question =
+                    model.question
+
+                updatedAnswers =
+                    toggleSpecificAnswer indexToToggle model.question.answers
+
+                updatedQuestion =
+                    { question | answers = updatedAnswers }
+
+                -- = { model.question | answers = ( toggleSpecificAnswer indexToToggle model.question.answers ) }
+            in
+            ( { model | question = updatedQuestion }, Cmd.none )
 
         Vote ->
             ( { model | display = Result }, Cmd.none )
@@ -117,42 +135,46 @@ renderAnswerButton index answer =
     button [ answerButtonClass answer.isSelected, onClick (ToggleAnswer index) ]
         [ text answer.text ]
 
+
+
 -- renderVoteGradient : Int -> String
 -- renderVoteGradient votes =
---     let percentage = model.answers in
+--     let percentage = model.question.answers in
 --         "linear-gradient(90deg, green 50%, white 50%);"
 --     -- "linear-gradient(90deg, green 50%, white 50%);"
+
+
 getVoteGradient : Int -> Answer -> String
 getVoteGradient index answer =
-    if Maybe.withDefault 0 (List.maximum (List.map .votes model.answers)) <= answer.votes then
-        "#B1FFBD " ++ toString ((toFloat answer.votes / toFloat (List.sum (List.map .votes model.answers))) * 100)
+    if Maybe.withDefault 0 (List.maximum (List.map .votes model.question.answers)) <= answer.votes then
+        "#B1FFBD " ++ toString ((toFloat answer.votes / toFloat (List.sum (List.map .votes model.question.answers))) * 100)
     else
-        "#FFB1B1 " ++ toString ((toFloat answer.votes / toFloat (List.sum (List.map .votes model.answers))) * 100)
+        "#FFB1B1 " ++ toString ((toFloat answer.votes / toFloat (List.sum (List.map .votes model.question.answers))) * 100)
 
 
 renderResultAnswer : Int -> Answer -> Html Msg
 renderResultAnswer index answer =
-    div [ answerButtonClass False, style [("background", "linear-gradient(90deg, " ++ (getVoteGradient index answer) ++ "%, white 0%)")] ]
+    div [ answerButtonClass False, style [ ( "background", "linear-gradient(90deg, " ++ getVoteGradient index answer ++ "%, white 0%)" ) ] ]
         [ --div [style [("width", "50%"), ("background", "#B1FFBD")]] []
-         span [] [text answer.text]
-        , span [ resultAnswerVotes ] [text ( toString answer.votes ++ " votes" ) ]
-    ]
+          span [] [ text answer.text ]
+        , span [ resultAnswerVotes ] [ text (toString answer.votes ++ " votes") ]
+        ]
 
 
 view : Model -> Html Msg
 view model =
     if model.display == Voting then
         div [ containerClass ]
-            ([ h1 [ titleClass ] [ text model.question ] ]
-                ++ List.indexedMap renderAnswerButton model.answers
+            ([ h1 [ titleClass ] [ text model.question.text ] ]
+                ++ List.indexedMap renderAnswerButton model.question.answers
                 ++ [ button [ createButtonClass, onClick Vote ] [ text "Vote" ]
-                   , div [] [ text model.questionId ]
+                   , div [] [ text model.question.id ]
                    ]
             )
     else
         div [ containerClass ]
-            ([ h1 [ titleClass ] [ text model.question ] ]
-                ++ List.indexedMap renderResultAnswer model.answers
-                ++ [ div [] [ text model.questionId ]
+            ([ h1 [ titleClass ] [ text model.question.text ] ]
+                ++ List.indexedMap renderResultAnswer model.question.answers
+                ++ [ div [] [ text model.question.id ]
                    ]
             )
