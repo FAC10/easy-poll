ee9a03d24a21a8702f7fc5fadab1569f97016c48
Matt Lubel <matt_lubel@hotmail.com>
2017-06-09 12:25:06 +0100

    #21 reformat model and poll.elm

--- elm-files/Poll.elm
+++ elm-files/Poll.elm
@@ -9,14 +9,20 @@ import Style exposing (..)
 -- MODEL
 
 
+type alias Question =
+    { id : String
+    , text : String
+    , answers : List String
+    }
+
+
 type Display
     = Create
     | Success
 
 
 type alias Model =
-    { question : String
-    , answers : List String
+    { question : Question
     , display : Display
     , url : String
     , hasEditedAnswers : Bool
@@ -25,8 +31,12 @@ type alias Model =
 
 model : Model
 model =
-    { question = ""
-    , answers = [ "", "" ]
+    { question =
+        { text = ""
+        , id = ""
+        , answers =
+            [ "", "" ]
+        }
     , display = Create
     , url = "www.easy-poll.co.uk/#p0ll1d"
     , hasEditedAnswers = False
@@ -37,7 +47,9 @@ yesNoWords =
     [ "am", "are", "is", "do", "does", "was", "were", "did", "will", "have", "can", "has", "could", "should", "may", "must", "dare", "ought", "shall", "might", "would" ]
 
 
+
 -- init and main
+
 
 init : ( Model, Cmd Msg )
 init =
@@ -53,6 +65,7 @@ main =
         }
 
 
+
 -- UPDATE
 
 
@@ -66,6 +79,10 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ChangeQuestion newQuestion ->
+            let
+                question =
+                    model.question
+            in
             if not model.hasEditedAnswers then
                 let
                     questionWords =
@@ -78,6 +95,7 @@ update msg model =
                         List.member "or" questionWords
                 in
                 if List.length (String.split " or " newQuestion) == 2 then
+                    -- if one "or" in the question
                     let
                         firstSection =
                             Maybe.withDefault "" (List.head (String.split " or " newQuestion))
@@ -95,33 +113,40 @@ update msg model =
                                         (Maybe.withDefault "" (List.head (String.split " " secondSection)))
                                     )
                                 )
+
+                        updatedQuestion =
+                            { question | text = newQuestion, answers = addOrOptions firstOption secondOption question.answers }
                     in
-                    ( { model | question = newQuestion, answers = addOrOptions firstOption secondOption model.answers }, Cmd.none)
+                    ( { model | question = updatedQuestion }, Cmd.none )
                 else if List.member (String.toLower firstWord) yesNoWords then
-                    --                    if List.isEmpty (List.filter (\a -> String.length a > 0) model.answers) then
+                    -- if question begins with a yes or no word
                     if not model.hasEditedAnswers then
-                        if List.length model.answers == 2 then
-                            ( { model | question = newQuestion, answers = addYesAndNo model.answers ++ [ "" ] }, Cmd.none )
+                        if List.length model.question.answers == 2 then
+                            ( { model | question = { question | text = newQuestion, answers = addYesAndNo model.question.answers ++ [ "" ] } }, Cmd.none )
                         else
-                            ( { model | question = newQuestion, answers = addYesAndNo model.answers }, Cmd.none )
+                            ( { model | question = { question | text = newQuestion, answers = addYesAndNo model.question.answers } }, Cmd.none )
                     else
-                        ( { model | question = newQuestion }, Cmd.none )
+                        ( { model | question = { question | text = newQuestion } }, Cmd.none )
                 else
-                    ( { model | question = newQuestion }, Cmd.none )
-            else if List.length (List.filter (\a -> not (a == "")) model.answers) == 0 then
-                ( { model | question = newQuestion, hasEditedAnswers = False }, Cmd.none )
+                    ( { model | question = { question | text = newQuestion } }, Cmd.none )
+            else if List.length (List.filter (\a -> not (a == "")) model.question.answers) == 0 then
+                -- if all answers are empty strings
+                ( { model | question = { question | text = newQuestion }, hasEditedAnswers = False }, Cmd.none )
             else
-                ( { model | question = newQuestion }, Cmd.none )
+                ( { model | question = { question | text = newQuestion } }, Cmd.none )
 
         ChangeAnswer index newAnswer ->
             let
                 updatedList =
-                    List.indexedMap (replaceAtIndexWith index newAnswer) model.answers
+                    List.indexedMap (replaceAtIndexWith index newAnswer) model.question.answers
+
+                question =
+                    model.question
             in
             if not (List.member "" updatedList) then
-                ( { model | answers = updatedList ++ [ "" ], hasEditedAnswers = True }, Cmd.none )
+                ( { model | question = { question | answers = updatedList ++ [ "" ] }, hasEditedAnswers = True }, Cmd.none )
             else
-                ( { model | answers = updatedList, hasEditedAnswers = True }, Cmd.none )
+                ( { model | question = { question | answers = updatedList }, hasEditedAnswers = True }, Cmd.none )
 
         CreatePoll ->
             -- request to api here
@@ -136,21 +161,16 @@ replaceAtIndexWith replaceIndex newItem currIndex item =
         item
 
 
+addOrOptions : String -> String -> List String -> List String
 addOrOptions opt1 opt2 list =
     List.indexedMap (replaceAtIndexWith 1 opt2)
         (List.indexedMap (replaceAtIndexWith 0 opt1) list)
 
 
+addYesAndNo : List String -> List String
 addYesAndNo list =
     List.indexedMap (replaceAtIndexWith 1 "no")
         (List.indexedMap (replaceAtIndexWith 0 "yes") list)
-
-
-
--- listIsEmpty : List -> Bool
--- listIsEmpty list =
---     List.isEmpty (List.filter String.isEmpty list)
--- VIEW
 
 
 generatePlaceholder : Int -> Attribute Msg
@@ -172,9 +192,9 @@ view model =
     if model.display == Create then
         div [ containerClass ]
             ([ h1 [ titleClass ] [ text "Easy Poll" ]
-             , textarea [ questionClass, placeholder "Your question here!", onInput ChangeQuestion ] [ text model.question ]
+             , textarea [ questionClass, placeholder "Your question here!", onInput ChangeQuestion ] [ text model.question.text ]
              ]
-                ++ List.indexedMap renderAnswerField model.answers
+                ++ List.indexedMap renderAnswerField model.question.answers
                 ++ [ button [ createButtonClass, onClick CreatePoll ] [ text "Create!" ]
                    ]
             )
