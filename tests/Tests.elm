642e834dbdb8f7c98cf038118e16e3205b9402ba
Matt Lubel <matt_lubel@hotmail.com>
2017-06-09 10:33:37 +0100

    add a couple of new tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -8,12 +8,26 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    describe "The Poll app"
+    describe "The Poll page"
         [ describe "replaceAtIndexWith"
             -- Nest as many descriptions as you like.
             [ test "does nothing when indexes don't match" <|
                 \_ ->
                     Poll.replaceAtIndexWith 0 "new" 1 "old"
                         |> Expect.equal "old"
+            , test "returns new value when indexes match" <|
+                \_ ->
+                    Poll.replaceAtIndexWith 3 "new" 3 "old"
+                        |> Expect.equal "new"
+            ]
+        , describe "addYesAndNo"
+            [ test "does nothing to empty list" <|
+                \_ ->
+                    Poll.addYesAndNo []
+                        |> Expect.equal []
+            , test "puts yes and no in list of 2 empty elements" <|
+                \_ ->
+                    Poll.addYesAndNo [ "", "" ]
+                        |> Expect.equal [ "yes", "no" ]
             ]
         ]
