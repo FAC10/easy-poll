153c59ba8abcaa4447db61c37aa1a41ee8f386f2
Matt Lubel <matt_lubel@hotmail.com>
2017-07-08 11:34:42 +0300

    fix indentation

--- server/server.js
+++ server/server.js
@@ -10,29 +10,29 @@ app.use(express.static('public'))
 
 // create a new poll
 app.post('/questions', (req, res) => {
-	// create id
-	console.log(req.body)
-	// add to db
-	// return id!!!
-	res.send('m0ckQu3st1OnId')
+  // create id
+  console.log(req.body)
+  // add to db
+  // return id!!!
+  res.send('m0ckQu3st1OnId')
 })
 
 // get poll for voting page
 app.get('/questions/:id', (req, res) => {
-	console.log(req.params.id)
-	// get question by id
-	// send back question
-	res.send('test')
+  console.log(req.params.id)
+  // get question by id
+  // send back question
+  res.send('test')
 })
 
 // handle vote and send back results
 app.post('/questions/:id/vote', (req, res) => {
-	// get question by id
-	console.log(req.params.id)
-	// get answer index from req body
-	console.log(req.body)
-	// update question
-	// send back question
+  // get question by id
+  console.log(req.params.id)
+  // get answer index from req body
+  console.log(req.body)
+  // update question
+  // send back question
 })
 
 module.exports = app
