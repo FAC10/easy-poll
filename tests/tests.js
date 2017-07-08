049468a56d87d76647bcc1670bb0c473ee9057c6
Matt Lubel <matt_lubel@hotmail.com>
2017-07-08 12:56:06 +0300

    #27 add basic integration test for home route

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,19 @@
 const tape = require('tape')
+const request = require('supertest')
 
 const app = require('../server/server.js')
 
-tape('basic passing test', t => {
-  t.ok(true, 'true should be truthy.')
-  t.end()
+tape('GET request to /', t => {
+  request(app)
+    .get('/')
+    .end((err, res) => {
+      if (err) throw err
+      t.equal(res.status, 200, 'should return status code 200')
+      t.ok(res.text.includes(
+        '<script type="text/javascript" src="./poll.js"></script>'),
+        'should return html page containing script for poll app'
+        )
+      t.end()
+    })
 })
+
