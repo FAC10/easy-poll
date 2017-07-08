aa14ab6751227cdcdaef859e69fa48313583178e
Matt Lubel <matt_lubel@hotmail.com>
2017-07-08 11:46:44 +0300

    #27 move route handlers to separate file

--- server/server.js
+++ server/server.js
@@ -2,37 +2,22 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const handlers = require('./handlers.js')
+
 const app = express()
 
 app.use(express.static(path.join(__dirname, '../public')))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
+// ROUTES
 // create a new poll
-app.post('/questions', (req, res) => {
-  // create id
-  console.log(req.body)
-  // add to db
-  // return id!!!
-  res.send('m0ckQu3st1OnId')
-})
+app.post('/questions', handlers.createPoll)
 
 // get poll for voting page
-app.get('/questions/:id', (req, res) => {
-  console.log(req.params.id)
-  // get question by id
-  // send back question
-  res.send('test')
-})
+app.get('/questions/:id', handlers.getPoll)
 
 // handle vote and send back results
-app.post('/questions/:id/vote', (req, res) => {
-  // get question by id
-  console.log(req.params.id)
-  // get answer index from req body
-  console.log(req.body)
-  // update question
-  // send back question
-})
+app.post('/questions/:id/vote', handlers.vote)
 
 module.exports = app
