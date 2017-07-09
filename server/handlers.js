093a8b846b202d426fcf83d9b9eb8921abef6a58
Matt Lubel <matt_lubel@hotmail.com>
2017-07-10 00:40:49 +0300

    add functionality for handlers #38

--- server/handlers.js
+++ server/handlers.js
@@ -1,25 +1,61 @@
+const MongoUtil = require('./mongoUtil.js')
+
 module.exports = handlers = {}
 
 handlers.createPoll = (req, res) => {
+  // receives json data for new poll
+  // adds to db
+  // returns poll
+  const db = MongoUtil.getDb()
+  const polls = db.collection('polls')
   // create id
-  console.log(req.body)
+  const poll = req.body
   // add to db
-  // return id!!!
-  res.send('m0ckQu3st1OnId')
+  polls.insert(poll, (err, result) => {
+    if (err) {
+      return res.send(err)
+    }
+    res.send(poll)
+  })
 }
 
 handlers.getPoll = (req, res) => {
-  console.log(req.params.id)
+  // receives id in url params
+  // returns poll data
+  const db = MongoUtil.getDb()
+  const polls = db.collection('polls')
+  const id = req.params.id
   // get question by id
-  // send back question
-  res.send('test')
+  polls.find({id}).toArray((err, polls) => {
+    if (err) {
+      return res.send(err)
+    } 
+    // sends back array for now
+    res.send(polls)
+  })
 }
 
 handlers.vote = (req, res) => {
-  // get question by id
-  console.log(req.params.id)
+  // receives answer index to increment inside req.body {index}
+  // gets poll index from url params
+  // increments answer vote count
+  // sends back updated poll
+  const db = MongoUtil.getDb()
+  const polls = db.collection('polls')
+  // get poll id from url
+  const id = req.params.id
   // get answer index from req body
-  console.log(req.body)
-  // update question
-  // send back question
+  const { index } = req.body
+  // update vote count for answer with given index
+  polls.findOneAndUpdate(
+    {id},
+    {$inc: { [`answers.${index}.votes`]: 1}},
+    {returnOriginal: false},
+    (err, poll) => {
+      if (err) {
+        return res.send(err)
+      }
+      // send back updated poll
+      res.send(poll)
+  })
 }
